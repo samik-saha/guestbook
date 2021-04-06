1e1f24ce40317114521ed79584667aa296df6741
Samik <3612424+samik-saha@users.noreply.github.com>
2021-04-06 19:58:39 -0400

    Created Dockerfile and application properties

--- README.md
+++ README.md
@@ -10,5 +10,21 @@ Endpoint    | Method| Request Body| Response
 /guestbook  |  POST | {"name":"abc", "comment":""}|
 
 
+## Instructions to run in Docker
+Create custom network
 
+    docker network create --driver bridge guestbook-net
 
+Run postgres instance
+
+    docker run --network guestbook-net -p 5432:5432 --name my-postgres -e POSTGRES_DB=guestbookdb -e POSTGRES_PASSWORD=open -d postgres
+
+Build Docker Image
+
+    docker build -t guestbook:dev .
+
+Run application in Docker container
+
+    docker run --name guestbook --network guestbook-net -e PORT=8080 -e SPRING_PROFILES_ACTIVE=docker -p 9000:8080 -d guestbook:dev
+
+    
--- src/test/java/com/galvanize/guestbook/service/GuestbookServiceTest.java
+++ src/test/java/com/galvanize/guestbook/service/GuestbookServiceTest.java
@@ -1,23 +1,22 @@
 package com.galvanize.guestbook.service;
 
 import com.galvanize.guestbook.dto.CommentDto;
-import com.galvanize.guestbook.repository.CommentRepository;
 import com.galvanize.guestbook.entity.CommentEntity;
-import com.galvanize.guestbook.service.GuestbookService;
+import com.galvanize.guestbook.repository.CommentRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class GuestbookServiceTest {
